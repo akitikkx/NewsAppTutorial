d8c6dce1c9a9f76d83dc0e44222ffbd918ebc72e
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2020-01-02 19:31:23 +0200

    step 2: Defining the network connection - added Article domain model and updated NetworkNewsHeadlineResponse.kt to convert the API response to domain model

--- app/src/main/java/com/ahmedtikiwa/mynewsapp/network/NetworkNewsHeadlinesResponse.kt
+++ app/src/main/java/com/ahmedtikiwa/mynewsapp/network/NetworkNewsHeadlinesResponse.kt
@@ -1,5 +1,6 @@
 package com.ahmedtikiwa.mynewsapp.network
 
+import com.ahmedtikiwa.mynewsapp.domain.Article
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
@@ -14,18 +15,33 @@ data class NetworkNewsHeadlinesResponse(
 
 @JsonClass(generateAdapter = true)
 data class NetworkArticle(
-    val author: String,
-    val content: String,
-    val description: String,
+    val author: String?,
+    val content: String?,
+    val description: String?,
     val publishedAt: String,
     val source: NetworkSource,
     val title: String,
     val url: String,
-    val urlToImage: String
+    val urlToImage: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class NetworkSource(
-    val id: Any,
-    val name: String
+    val id: String?,
+    val name: String?
 )
+
+fun NetworkNewsHeadlinesResponseContainer.asDomainModel(): List<Article> {
+    return articles.map { networkArticle ->
+        Article(
+            url = networkArticle.url,
+            author = networkArticle.author,
+            content = networkArticle.content,
+            description = networkArticle.description,
+            publishedAt = networkArticle.publishedAt,
+            source = networkArticle.source.name,
+            title = networkArticle.title,
+            urlToImage = networkArticle.urlToImage
+        )
+    }
+}
